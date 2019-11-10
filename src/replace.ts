cd402ce2720224aa03632f3ed5c9fd2278ec7bbd
Christopher Schleiden <cschleiden@live.de>
2019-11-09 18:58:36 -0800

    Run prettier

--- src/replace.ts
+++ src/replace.ts
@@ -1,14 +1,23 @@
 import replace from "replace-in-file";
 
-export async function replaceTokens(tokenPrefix: string, tokenSuffix: string, files: string[]) {
-  const fromRegEx = new RegExp(`${escapeDelimiter(tokenPrefix)}(.+?)${escapeDelimiter(tokenSuffix)}`, "gm");
-  const matchRegEx = new RegExp(`${escapeDelimiter(tokenPrefix)}(.+?)${escapeDelimiter(tokenSuffix)}`);
+export async function replaceTokens(
+  tokenPrefix: string,
+  tokenSuffix: string,
+  files: string[]
+) {
+  const fromRegEx = new RegExp(
+    `${escapeDelimiter(tokenPrefix)}(.+?)${escapeDelimiter(tokenSuffix)}`,
+    "gm"
+  );
+  const matchRegEx = new RegExp(
+    `${escapeDelimiter(tokenPrefix)}(.+?)${escapeDelimiter(tokenSuffix)}`
+  );
 
   const result = await replace({
     files,
     allowEmptyPaths: true,
     from: fromRegEx,
-    to: (match) => {
+    to: match => {
       const m = match.match(matchRegEx);
       if (m) {
         const tokenName = m[1];
@@ -23,5 +32,5 @@ export async function replaceTokens(tokenPrefix: string, tokenSuffix: string, fi
 }
 
 function escapeDelimiter(delimiter: string): string {
-  return delimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+  return delimiter.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
 }
